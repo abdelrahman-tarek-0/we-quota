4f239c4fe97c170c70b80e05143b5ac8690b7794
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-03-14 20:35:57 +0200

    added prettier and eslint for formating

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,11 @@
-const login = require("./modules/login");
-const mainPage = require("./modules/mainPage");
+const login = require('./modules/login')
+const mainPage = require('./modules/mainPage')
 
-const app = () => {
-  return login()
-    .then(mainPage)
-    .then((data) => {
-      console.log(data);
-    })
-};
+const app = () =>
+   login()
+      .then(mainPage)
+      .then((data) => {
+         console.log(data)
+      })
 
 app()
--- src/config/config.js
+++ src/config/config.js
@@ -1,60 +1,56 @@
-const fs = require("fs");
-require("dotenv").config();
+const fs = require('fs')
+require('dotenv').config()
 
-const url = "https://api-my.te.eg/api";
-const preTokenUrl = `${url}/user/generatetoken?channelId=WEB_APP`;
-const loginUrl = `${url}/user/login?channelId=WEB_APP`;
-const usageUrl = `${url}/line/freeunitusage`;
+const url = 'https://api-my.te.eg/api'
+const preTokenUrl = `${url}/user/generatetoken?channelId=WEB_APP`
+const loginUrl = `${url}/user/login?channelId=WEB_APP`
+const usageUrl = `${url}/line/freeunitusage`
 const balanceUrl = `${url}/line/balance`
-const file = `${__dirname}/config.json`;
+const file = `${__dirname}/config.json`
 
 const db = {
-  read: () => {
-    try {
-      return JSON.parse(fs.readFileSync(file));
-    } catch (error) {
-      throw error;
-    }
-  },
-  write: (data) => {
-    try {
-      return fs.writeFileSync(file, JSON.stringify(data));
-    } catch (error) {
-      throw error;
-    }
-  },
-};
-const { password, number } = db.read();
+   read: () => {
+      try {
+         return JSON.parse(fs.readFileSync(file))
+      } catch (error) {
+         throw error
+      }
+   },
+   write: (data) => {
+      try {
+         return fs.writeFileSync(file, JSON.stringify(data))
+      } catch (error) {
+         throw error
+      }
+   },
+}
+const { password, number } = db.read()
 const userAuth = {
-    number,
-    password
+   number,
+   password,
 }
 
-const getSession = ()=>{
-  return db.read().session
-}
-const setSession = (token,customerId,customerName)=>{
-  const iat = Math.floor(Date.now()/1000)  
-  const exp = Math.floor(Date.now()/1000) + 3.5*60*60 
-  const password = userAuth.password
-  const number = userAuth.number
-  console.log(userAuth.browserPath);
-  db.write({
-    password,
-    number,
-    browserPath:userAuth.browserPath,
-    session:{
-      customerId,
-      customerName,
-      iat,
-      exp,
-      token
-    }
-  })
+const getSession = () => db.read().session
+const setSession = (token, customerId, customerName) => {
+   const iat = Math.floor(Date.now() / 1000)
+   const exp = Math.floor(Date.now() / 1000) + 3.5 * 60 * 60
+   const { password } = userAuth
+   const { number } = userAuth
+   console.log(userAuth.browserPath)
+   db.write({
+      password,
+      number,
+      browserPath: userAuth.browserPath,
+      session: {
+         customerId,
+         customerName,
+         iat,
+         exp,
+         token,
+      },
+   })
 }
 // your_password
 // 022123456
 
-
-
-module.exports = { preTokenUrl, loginUrl, usageUrl,balanceUrl,db,userAuth};
+module.exports = { preTokenUrl, loginUrl, usageUrl, balanceUrl, db, userAuth }
