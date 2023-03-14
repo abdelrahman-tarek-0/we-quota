3ef3992646f9d0a90884ffae0596ac17a6b6b002
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-03-14 10:25:51 +0200

    login into we works

---
+++ src/app/modules/login.js
@@ -0,0 +1,28 @@
+const { login } = require("we-te-login");
+const { userAuth } = require("../../config/config");
+
+module.exports = async () => {
+  const res = await login(userAuth.password, userAuth.number);
+
+  const {token,customerId,customerName} = res;
+
+  const payload = {
+    body: {},
+    header: {
+      msisdn: userAuth.number,
+      customerId,
+      numberServiceType: "FBB",
+      locale: "en",
+    },
+  };
+  const headers = {
+    Jwt: token,
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+    Host: "api-my.te.eg",
+    Origin: "https://my.te.eg",
+    Referer: "https://my.te.eg/",
+  };
+  
+  return {headers,payload,customerName}
+};
