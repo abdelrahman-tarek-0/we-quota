cc96c8f46fbab8a90f5f875df6624175ad3e4d7e
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-03-14 09:04:35 +0200

    add simple db config

--- src/config/config.js
+++ src/config/config.js
@@ -6,10 +6,32 @@ const preTokenUrl = `${url}/user/generatetoken?channelId=WEB_APP`;
 const loginUrl = `${url}/user/login?channelId=WEB_APP`;
 const usageUrl = `${url}/line/freeunitusage`;
 const balanceUrl = `${url}/line/balance`
+const file = `${__dirname}/config.json`;
 
+const db = {
+  read: () => {
+    try {
+      return JSON.parse(fs.readFileSync(file));
+    } catch (error) {
+      throw error;
+    }
+  },
+  write: (data) => {
+    try {
+      return fs.writeFileSync(file, JSON.stringify(data));
+    } catch (error) {
+      throw error;
+    }
+  },
+};
+const { password, number } = db.read();
+const userAuth = {
+    number,
+    password
+}
 // your_password
 // 0221234567
 
 
 
-module.exports = { preTokenUrl, loginUrl, usageUrl,balanceUrl};
+module.exports = { preTokenUrl, loginUrl, usageUrl,balanceUrl,db,userAuth};
