f746d0e85a2182b8cbd6e2f502e85542718c28c3
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-03-14 22:06:14 +0200

    added validator

--- src/app/modules/mainPage.js
+++ src/app/modules/mainPage.js
@@ -1,19 +1,14 @@
 const axios = require('axios')
+const validator = require('../utils/validator')
 const { usageUrl, balanceUrl } = require('../../config/config')
 
 module.exports = async ({ headers, payload, customerName }) => {
    const usageRes = await axios.post(usageUrl, payload, { headers })
-
    const balanceRes = await axios.post(balanceUrl, payload, { headers })
-   // let balanceRes = { data: { body: { balance: null, msisdn: null } } };
 
-   if (
-      !usageRes?.data?.body?.detailedLineUsageList ||
-      !usageRes?.data?.body?.summarizedLineUsageList ||
-      !balanceRes?.data?.body
-   )
+   if (!validator.usageBalance(usageRes,balanceRes))
       throw new Error('Error happened getting usage info please re login')
-
+   
    return {
       balance: {
          ...balanceRes?.data?.body,
---
+++ src/app/utils/validator.js
@@ -0,0 +1,12 @@
+module.exports ={
+    usageBalance:(usageRes,balanceRes) => {
+        if (
+           !usageRes?.data?.body?.detailedLineUsageList ||
+           !usageRes?.data?.body?.summarizedLineUsageList ||
+           !balanceRes?.data?.body
+        ){
+           return false
+        }
+        return true
+     }
+} 
